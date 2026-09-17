b3a3e7bc5cdc83efba1db288e9b880c08aba8158
matostomi <matostomi773@gmail.com>
2026-09-17 13:09:09 +0300

    Rename 'DELIVERED' to 'OPERATION_FULLY_COMPLETED'

--- backend/shipments/models.py
+++ backend/shipments/models.py
@@ -20,7 +20,7 @@ class Shipment(models.Model):
         WAITING_CARGO_RELEASE = 'waiting_cargo_release', 'Waiting Cargo Release'
         FACTORY_UNLOADING = 'factory_unloading', 'Factory Unloading'
         EMPTY_CONTAINER_RETURNED = 'empty_container_returned', 'Empty Container Returned'
-        DELIVERED = 'delivered', 'Completed'
+        OPERATION_FULLY_COMPLETED = 'OPERATION_FULLY_COMPLETED', 'Completed'
         CANCELLED = 'cancelled', 'Cancelled'
 
     class Priority(models.TextChoices):
